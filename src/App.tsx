432d7b4bcf6d974f39ec1a2cfe8e30a4594ebc40
Daniel Bezerra <bezerradaniels@outlook.com>
2025-12-03 13:28:07 -0300

    versao sem admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { PlansProvider } from './context/PlansContext';
 import { TeamProvider } from './context/TeamContext';
 import { GalleryProvider } from './context/GalleryContext';
@@ -9,11 +9,9 @@ function App() {
     <GalleryProvider>
       <TeamProvider>
         <PlansProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />              
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />              
+          </Routes>
         </PlansProvider>
       </TeamProvider>
     </GalleryProvider>
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 import '@fontsource/stack-sans-headline/300.css';
 import '@fontsource/stack-sans-headline/400.css';
 import '@fontsource/stack-sans-headline/500.css';
 import '@fontsource/stack-sans-headline/600.css';
 import '@fontsource/stack-sans-headline/700.css';
-import App from './App.tsx'
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename="/new">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
